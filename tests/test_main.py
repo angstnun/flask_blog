b2315b65aa7657988ca24650013088f1125e4221
Stef Nunez <angstnun@gmail.com>
2019-09-25 00:42:00 -0400

    Removed basic testing unit tests and created unit test to get the root of a flask app

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,31 +1,23 @@
 import unittest
 from selenium import webdriver
+from flaskr import create_app
 
-class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+class AppTest(unittest.TestCase):
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def setUp(self):
+        self.app = create_app({
+            "TESTING": True
+        })
+        self.client = self.app.test_client()
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-            
-    def test_radiohead(self):
-        result = 2 + 2
-        self.assertEqual(result, 4, "Radiohead logic doesn't work here")
-        
-    def test_shouldDisplayHomePage(self):
-        browser = webdriver.Chrome()
-        browser.get('http://localhost:5000')
-        isFlaskr = browser.title.upper().find("FLASKR") > -1
-        self.assertTrue(isFlaskr)
+    def tearDown(self):
+        pass
+
+    def test_CheckIfRootReturns200(self):
+        response = self.client.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
